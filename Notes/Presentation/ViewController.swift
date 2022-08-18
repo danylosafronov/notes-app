35e41967374b444278977920a7892e94cf54103a
Danylo <danylosafronov@gmail.com>
2022-08-18 16:21:30 +0300

    Initial Commit

---
+++ Notes/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Danylo Safronov on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
